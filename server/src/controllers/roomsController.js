3c08ec3f23db30eaf1675cb81b4b092f21433564
Welisson luca <welissonluca17@gmail.com>
2021-05-21 13:02:27 -0300

    feat: get and set rooms in roomscontrollers and updatedRoom cheking user isSpeaker and Count users room

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,5 +1,7 @@
 import { constants } from "../util/constants.js";
-import Attende from "../entities/attendee.js"
+import Attendee from "../entities/attendee.js"
+import Room from "../entities/room.js";
+
 export default class RoomsController {
 	#users = new Map();
 	constructor() {
@@ -22,24 +24,54 @@ export default class RoomsController {
 			roomId
 		)
 		console.log(updatedUserData)
+		const updatedRoom = this.#joinUserRoom(socket, updatedUserData, room)
+		console.log(updatedRoom)
 		socket.emit(constants.event.USER_CONNECTED, updatedUserData)
 	}
 	#joinUserRoom(socket, user, room) {
 		const roomId = room.id
 		const existingRoom = this.rooms.has(roomId);
 		const currentRoom = existingRoom ? this.rooms.get(roomId) : {}
-		const currentUser = new Attende({
+		const currentUser = new Attendee({
 			...user,
 			roomId
 		})
+	const [owner, users] = existingRoom
+		? [currentRoom.owner, currentUser.users]
+		: [currentUser, new Set()];
 		
-		
+		const updateRoom = this.#mapRoom({
+			...currentRoom,
+			...room,
+			owner,
+			users: new Set([...users, ...[currentUser]])
+		})
+
+		this.rooms.set(roomId, updateRoom)
+
+		socket.join(roomId)
+
+		return this.rooms.get(roomId)
+
+	}
+	#mapRoom(room) {
+		const users = [...room.users.values()];
+		const speakersCount = users.filter(user => user.isSpeaker).length
+		const featuredAttendees = users.slice(0, 3)
+		const mappedRoom = new Room({
+			...room,
+			featuredAttendees,
+			speakersCount,
+			attendesCount: room.users.size
+		})
+
+		return mappedRoom
 	}
 	#updateGlobalUserData(userId, userData = {}, roomId = '') {
 		const user = this.#users.get(userId) ?? {}
 		const existingRoom = this.rooms.has(roomId)
 
-		const updatedUserData = new Attende({
+		const updatedUserData = new Attendee({
 			...user,
 			...userData,
 			roomId,
--- server/src/entities/room.js
+++ server/src/entities/room.js
@@ -1,11 +1,12 @@
+import Attendee from "./attendee.js"
 export default class Room{
   constructor({ id, topic, attendesCount, speakersCount, featuredAttendees, owner, users}) {
     this.id = id;
     this.topic = topic
     this.attendesCount = attendesCount
     this.speakersCount = speakersCount
-      this.featuredAttendees = featuredAttendees
-      this.owner = owner
-      this.users = users
+    this.featuredAttendees = featuredAttendees?.map(attendee => new Attendee(attendee))
+    this.owner = new Attendee(owner)
+    this.users = users
   }
 } 
