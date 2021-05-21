fcf610a959446ce9f582006f4d5d08b077bac65b
Welisson luca <welissonluca17@gmail.com>
2021-05-21 10:49:02 -0300

    feat: add entities in server and create private property updateGlobalUserData checking for user exists

--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,10 +1,40 @@
+import { constants } from "../util/constants.js";
 export default class RoomsController {
-	constructor() {}
+	#users = new Map();
+	constructor() {
+		this.rooms = new Map();
+	 }
 
 	onNewConnection(socket) {
 		const { id } = socket;
 
 		console.log("connection stablished with ", id);
+		this.#updateGlobalUserData(id)
+	}
+	joinRoom(socket, {user, ...room}) {
+		const userId = user.id = socket.id
+		const roomId = room.id
+
+		const updatedUserData = this.#updateGlobalUserData(
+			userId,
+			user,
+			roomId
+		)
+		socket.emit(constants.event.USER_CONNECTED, data)
+	}
+	#updateGlobalUserData(userId, userData = {}, roomId = '') {
+		const user = this.#users.get(userId) ?? {}
+		const existingRoom = this.rooms.has(roomId)
+
+		const updatedUserData = new Attende({
+			...user,
+			...userData,
+			roomId,
+			isSpeaker: !existingRoom,
+
+		})
+		this.#users.set(userId, updatedUserData)
+
 	}
 	getEvents() {
 		const functions = Reflect.ownKeys(RoomsController.prototype)
