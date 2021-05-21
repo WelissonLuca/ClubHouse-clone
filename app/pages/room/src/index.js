19b12891b573489e952481816df7e4e7bba802e5
Welisson luca <welissonluca17@gmail.com>
2021-05-21 13:21:25 -0300

    feat: notify all users and new user in looby

--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,27 +1,26 @@
-import { constants } from "../../_shared/constants.js";
-import SocketBuilder from "../../_shared/socketBuilder.js";
+import { constants } from "../../_shared/constants.js"
+import RoomSocketBuilder from "./util/roomSocket.js"
 
-const socketBuilder = new SocketBuilder({
-	socketUrl: constants.socketUrl,
-	namespace: constants.socketNamesSpaces.room,
-});
+
+const socketBuilder = new RoomSocketBuilder({
+    socketUrl: constants.socketUrl,
+    namespace: constants.socketNamespaces.room
+})
 
 const socket = socketBuilder
-	.setOnUserConnected(user => console.log("user connected", user))
-	.setOnUserDisconnected(user => console.log("user disconnected", user))
-	.build();
+    .setOnUserConnected((user) => console.log('user connected!', user))
+    .setOnUserDisconnected((user) => console.log('user disconnected!', user))
+    .setOnRoomUpdated((room) => console.log('room list!', room))
+    .build()
 
 const room = {
-	id: Date.now(),
-	topic: "JS Expert éh noix",
-};
+    id: '0001',
+    topic: 'JS Expert éh noix'
+}
 
 const user = {
-	img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/pilot_traveller_person_avatar-256.png",
-	username: "Welisson Luca",
-};
+    img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
+    username: 'Erick ' + Date.now()
+}
 
-socket.emit(constants.events.JOIN_ROOM, {
-	user,
-	room,
-});
+socket.emit(constants.events.JOIN_ROOM, { user, room })
--- server/src/controllers/roomsController.js
+++ server/src/controllers/roomsController.js
@@ -1,92 +1,115 @@
-import { constants } from "../util/constants.js";
 import Attendee from "../entities/attendee.js"
-import Room from "../entities/room.js";
+import Room from "../entities/room.js"
+import { constants } from "../util/constants.js"
 
 export default class RoomsController {
-	#users = new Map();
-	constructor() {
-		this.rooms = new Map();
-	 }
+    #users = new Map()
 
-	onNewConnection(socket) {
-		const { id } = socket;
+    constructor() {
+        this.rooms = new Map()
+    }
 
-		console.log("connection stablished with ", id);
-		this.#updateGlobalUserData(id)
-	}
-	joinRoom(socket, {user,room}) {
-		const userId = user.id = socket.id
-		const roomId = room.id
+    onNewConnection(socket) {
+        const { id } = socket
+        console.log('connection stablished with', id)
+        this.#updateGlobalUserData(id)
+    }
+    joinRoom(socket, { user, room }) {
 
-		const updatedUserData = this.#updateGlobalUserData(
-			userId,
-			user,
-			roomId
-		)
-		console.log(updatedUserData)
-		const updatedRoom = this.#joinUserRoom(socket, updatedUserData, room)
-		console.log(updatedRoom)
-		socket.emit(constants.event.USER_CONNECTED, updatedUserData)
-	}
-	#joinUserRoom(socket, user, room) {
-		const roomId = room.id
-		const existingRoom = this.rooms.has(roomId);
-		const currentRoom = existingRoom ? this.rooms.get(roomId) : {}
-		const currentUser = new Attendee({
-			...user,
-			roomId
-		})
-	const [owner, users] = existingRoom
-		? [currentRoom.owner, currentUser.users]
-		: [currentUser, new Set()];
-		
-		const updateRoom = this.#mapRoom({
-			...currentRoom,
-			...room,
-			owner,
-			users: new Set([...users, ...[currentUser]])
-		})
+        const userId = user.id = socket.id
+        const roomId = room.id
 
-		this.rooms.set(roomId, updateRoom)
+        const updatedUserData = this.#updateGlobalUserData(
+            userId,
+            user,
+            roomId
+        )
 
-		socket.join(roomId)
+        const updatedRoom = this.#joinUserRoom(socket, updatedUserData, room)
+        this.#notifyUsersOnRoom(socket, roomId, updatedUserData)
+        this.#replyWithActiveUsers(socket, updatedRoom.users)
+    }
+    #replyWithActiveUsers(socket, users) {
+        const event = constants.event.LOBBY_UPDATED
+        socket.emit(event, [...users.values()])
+    }
+    #notifyUsersOnRoom(socket, roomId, user) {
+        const event = constants.event.USER_CONNECTED
+        socket.to(roomId).emit(event, user)
+    }
 
-		return this.rooms.get(roomId)
+    #joinUserRoom(socket, user, room) {
+        const roomId = room.id
+        const existingRoom = this.rooms.has(roomId)
+        const currentRoom = existingRoom ? this.rooms.get(roomId) : {}
+        const currentUser = new Attendee({
+            ...user,
+            roomId
+        })
 
-	}
-	#mapRoom(room) {
-		const users = [...room.users.values()];
-		const speakersCount = users.filter(user => user.isSpeaker).length
-		const featuredAttendees = users.slice(0, 3)
-		const mappedRoom = new Room({
-			...room,
-			featuredAttendees,
-			speakersCount,
-			attendesCount: room.users.size
-		})
+        // definir quem é o dono da sala
+        const [owner, users] = existingRoom ?
+            [currentRoom.owner, currentRoom.users] :
+            [currentUser, new Set()]
 
-		return mappedRoom
-	}
-	#updateGlobalUserData(userId, userData = {}, roomId = '') {
-		const user = this.#users.get(userId) ?? {}
-		const existingRoom = this.rooms.has(roomId)
+        const updatedRoom = this.#mapRoom({
+            ...currentRoom,
+            ...room,
+            owner,
+            users: new Set([...users, ...[currentUser]])
+        })
 
-		const updatedUserData = new Attendee({
-			...user,
-			...userData,
-			roomId,
-			isSpeaker: !existingRoom,
+        this.rooms.set(roomId, updatedRoom)
 
-		})
+        socket.join(roomId)
 
-		this.#users.set(userId, updatedUserData)
-		return this.#users.get(userId)
-	}
-	getEvents() {
-		const functions = Reflect.ownKeys(RoomsController.prototype)
-			.filter(fn => fn != "constructor")
-			.map(name => [name, this[name].bind(this)]);
+        return this.rooms.get(roomId)
+    }
 
-		return new Map(functions);
-	}
+    #mapRoom(room) {
+        const users = [...room.users.values()]
+        const speakersCount = users.filter(user => user.isSpeaker).length
+        const featuredAttendees = users.slice(0, 3)
+        const mappedRoom = new Room({
+            ...room,
+            featuredAttendees,
+            speakersCount,
+            attendeesCount: room.users.size
+        })
+
+        return mappedRoom
+    }
+    #updateGlobalUserData(userId, userData = {}, roomId = '') {
+        const user = this.#users.get(userId) ?? {}
+        const existingRoom = this.rooms.has(roomId)
+
+        const updatedUserData = new Attendee({
+            ...user,
+            ...userData,
+            roomId,
+            // se for o unico na sala
+            isSpeaker: !existingRoom,
+        })
+
+        this.#users.set(userId, updatedUserData)
+
+        return this.#users.get(userId)
+
+    }
+
+    getEvents() {
+        const functions = Reflect.ownKeys(RoomsController.prototype)
+            .filter(fn => fn !== 'constructor')
+            .map(name => [name, this[name].bind(this)])
+
+        return new Map(functions)
+
+        /*
+            [
+                ['onNewConnection', this.onNewConnection],
+                ['disconnect', this.disconnect],
+            ]
+        */
+
+    }
 }
