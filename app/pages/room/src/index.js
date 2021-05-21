4e965c475464ce44ece89b3994b6375a06a909b3
Welisson luca <welissonluca17@gmail.com>
2021-05-21 02:12:16 -0300

    feat:  add src room

---
+++ app/pages/room/src/index.js
@@ -0,0 +1,13 @@
+
+
+
+const room = {
+  id: Date.now(),
+  topic: "JS Expert éh noix"
+}
+
+const user = {
+	img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/pilot_traveller_person_avatar-256.png",
+	username: "Welisson Luca",
+};
+
